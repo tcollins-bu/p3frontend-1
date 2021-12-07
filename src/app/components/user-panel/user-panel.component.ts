119e3c571a94926f5a005052d5bff294496b7d3f
jquick5 <jay.quick79@gmail.com>
2021-12-06 19:02:17 -0800

    Feed only shows posts by people you are following

--- src/app/components/user-panel/user-panel.component.ts
+++ src/app/components/user-panel/user-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Users } from 'src/app/models/user';
+import { FollowerService } from 'src/app/services/follower.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,26 +9,57 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-panel.component.scss'],
 })
 export class UserPanelComponent implements OnInit {
+  followers = [];
+  following = [];
   user: Users;
   fullName: string = null;
   age: number;
   job: string;
-  constructor(private uService: UserService) {}
+  userId: number;
+  numFollowers: number = 0;
+  numFollowed: number = 0;
+
+  constructor(
+    private uService: UserService,
+    private fService: FollowerService
+  ) {}
 
   ngOnInit(): void {
-    this.getUser();
+    this.getId();
+    setTimeout(() => {
+      this.getUser();
+      this.getFollowInfo();
+    }, 500);
   }
 
   getUser() {
+    this.uService.getUserById(this.userId).subscribe((user) => {
+      this.user = user;
+      this.fullName = `${this.user.firstName} ${this.user.lastName}`;
+      this.age = this.user.age;
+      this.job = this.user.jobTitle;
+    });
+  }
+
+  getId() {
     setTimeout(() => {
-      this.uService
-        .getUserById(Number(localStorage.getItem('userId')))
-        .subscribe((user) => {
-          this.user = user;
-          this.fullName = `${this.user.firstName} ${this.user.lastName}`;
-          this.age = this.user.age;
-          this.job = this.user.jobTitle;
-        });
-    }, 10);
+      this.userId = Number(localStorage.getItem('userId'));
+    }, 400);
+  }
+
+  getFollowInfo() {
+    this.fService.getFollowers().subscribe((followers) => {
+      this.followers = followers;
+      this.followers.forEach((f) => {
+        if (f.followedId == this.userId && f.followerId != this.userId) {
+          this.numFollowers++;
+        } else if (f.followerId == this.userId) {
+          this.numFollowed++;
+        }
+      });
+
+      console.log(this.numFollowers);
+      console.log(this.numFollowed);
+    });
   }
 }
